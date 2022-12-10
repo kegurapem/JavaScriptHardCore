98af9790b1aa97e8570f15bcb16f2548c0ad355c
kevin harold <harold.dev1993@gmail.com>
2022-12-10 12:03:18 -0500

    avanzando algo de recursividad

--- 04-EstructuraDeDatos-I/index.js
+++ 04-EstructuraDeDatos-I/index.js
@@ -12,3 +12,25 @@ function cuentaRegresiva(num) {
 }
 
 cuentaRegresiva(10)
+
+//************************************//
+function factorial(num) {
+    if (num == 0) return 1
+
+    return num * factorial(num - 1)
+}
+
+// input 5
+// 5 * factorial(4)
+// 4 * factorial(3)
+// 3 * factorial(2)
+// 2 * factorial(1)
+// 1 * factorial(0)
+// 1
+
+// 1                    1
+// 
+// 
+// 
+// 
+// 4 * 3 * 2 * 1 * factorial(0)
